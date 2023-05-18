b65e083a20d9f5e3664bc987662f4949c4c55b1e
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-05-17 20:40:43 -0500

    Set up server

--- index.js
+++ index.js
@@ -3,5 +3,14 @@
 require('dotenv').config();
 const { start } = require('./src/server');
 const PORT = process.env.PORT;
+const { sequelizeDatabase } = require('./src/models');
 
+// SYNC
+sequelizeDatabase.sync()
+  .then(() => {
+    console.log('Successfull Conection');
+  })
+  .catch(error => console.error(error));
+  
+  
 start(PORT);
---
+++ src/models/food.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = (sequelizeDatabase, DataTypes) =>{
+  return sequelizeDatabase.define('food', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    calories: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    group: {
+      type: DataTypes.ENUM,
+      values: ['fruit', 'vegie', 'meat', 'grains'],
+    },
+  });
+};
